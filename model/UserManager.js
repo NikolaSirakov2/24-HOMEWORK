5daeced311c1d2c51f1275e91634ca2903cd0acb
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-15 14:30:46 +0200

    Update UserManager.js

--- model/UserManager.js
+++ model/UserManager.js
@@ -1,51 +1,63 @@
 class User {
-    constructor(name, pass) {
-      (this.name = name), (this.pass = pass);
+  constructor(name, pass) {
+    (this.name = name), (this.pass = pass);
+  }
+}
+
+class UserManager {
+  constructor() {
+    let logedUser = JSON.parse(localStorage.getItem("loged")) || null;
+    if (logedUser) {
+      this.logedUser = new User(logedUser.name, logedUser.pass);
     }
   }
-  
-  class UserManager {
-    constructor() {
-      let logedUser = JSON.parse(localStorage.getItem("loged")) || null;
-      if (logedUser) {
-        this.logedUser = new User(logedUser.name, logedUser.pass);
-      }
-    }
-  
-    usersList = JSON.parse(localStorage.getItem("usersList")) || [];
-    logedUser = null;
-  
-    logIn = (name, pass) => {
-      let existingUser = this.usersList.filter(
-        (e) => e.name === name && e.pass === pass
-      );
-  
-      if (existingUser.length > 0) {
-        this.logedUser = existingUser;
-        let headerUserSign = document.getElementById('userLink'); 
-        headerUserSign.innerText = this.logedUser[0].name;
-        localStorage.setItem("loged", JSON.stringify(this.logedUser));
-        console.log(window.location.hash);
-        window.location.hash = "home";
-        return true;
-      }
-  
-      return false;
-    };
-  
-    logOut = () => {
-      localStorage.removeItem("loged");
-      let headerUserSign = document.getElementById('userLink'); 
-        headerUserSign.innerText = "User";
-    };
-  
-    createUser = (name, pass, confirm) => {
-      let existingUser = this.usersList.filter((e) => e.name === name);
+
+  usersList = JSON.parse(localStorage.getItem("usersList")) || [];
+  logedUser = null;
+
+  logIn = (name, pass) => {
+    let existingUser = this.usersList.filter(
+      (e) => e.name === name && e.pass === pass
+    );
+
+    if (existingUser.length > 0) {
+      this.logedUser = existingUser;
      
+      localStorage.setItem("loged", JSON.stringify(this.logedUser));
+      console.log(window.location.hash);
+      window.location.hash = "home";
+      return true;
+    } else {
+        let loginButton = document.getElementById("loginButt");
+        let popup = document.getElementById("loginPopUp");
+        openPopUp = () => {
+          popup.style.display = "inline-block";
+        };
   
-      if (existingUser.length === 0) {
-        this.usersList.push(new User(name, pass));
-        localStorage.setItem("usersList", JSON.stringify(this.usersList));
-      } 
-    };
-  }
+        loginButton.addEventListener("click", openPopUp());
+    }
+
+    return false;
+  };
+
+  logOut = () => {
+    localStorage.removeItem("loged");
+    let headerUserSign = document.getElementById("userLink");
+    headerUserSign.innerText = "User";
+  };
+
+  createUser = (name, pass, confirm) => {
+    let existingUser = this.usersList.filter((e) => e.name === name);
+
+    if (existingUser.length === 0 && pass === confirm) {
+      this.usersList.push(new User(name, pass));
+      localStorage.setItem("usersList", JSON.stringify(this.usersList));
+    } else {
+      openRegPopUp = () => {
+        registrationPopUp.style.display = "inline-block";
+      };
+
+      registerButt.addEventListener("click", openRegPopUp());
+    }
+  };
+}
