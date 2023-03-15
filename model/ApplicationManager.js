7bece5078c44355ad9418a65f15e24ccbce7aa14
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-15 17:15:43 +0200

    Create ApplicationManager.js

---
+++ model/ApplicationManager.js
@@ -0,0 +1,20 @@
+class Application {
+    constructor(id, name, income, amount, period){
+        this.id = id,
+        this.name = name,
+        this.income = income,
+        this.amount = amount,
+        this.period = period
+    }
+}
+
+class ApplicationManager {
+
+    applicationsList = [];
+
+    createApplication = (id, name, income, amount, period) => {
+        this.applicationsList.push(new Application(id, name, income, amount, period));
+        localStorage.setItem("applicationsList", JSON.stringify(this.applicationsList))
+    }
+
+}
