4393bdc374b3c71f82cd02ba093aada095d99dd8
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-15 11:20:51 +0200

    Create UserManager.js

---
+++ model/UserManager.js
@@ -0,0 +1,48 @@
+class User {
+    constructor(name, pass) {
+      (this.name = name), (this.pass = pass);
+    }
+  }
+  
+  class UserManager {
+    constructor() {
+      let logedUser = JSON.parse(localStorage.getItem("loged")) || null;
+      if (logedUser) {
+        this.logedUser = new User(logedUser.name, logedUser.pass);
+      }
+    }
+  
+    usersList = JSON.parse(localStorage.getItem("usersList")) || [];
+    logedUser = null;
+  
+    logIn = (name, pass) => {
+      let existingUser = this.usersList.filter(
+        (e) => e.name === name && e.pass === pass
+      );
+  
+      if (existingUser.length > 0) {
+        this.logedUser = existingUser;
+        let headerUserSign = document.getElementById('userLink');
+        console.log(this.logedUser.name);
+        headerUserSign.innerText = this.logedUser.name;
+        localStorage.setItem("loged", JSON.stringify(this.logedUser));
+        return true;
+      }
+  
+      return false;
+    };
+  
+    logOut = () => {
+      localStorage.removeItem("loged");
+    };
+  
+    createUser = (name, pass, confirm) => {
+      let existingUser = this.usersList.filter((e) => e.name === name);
+     
+  
+      if (existingUser.length === 0) {
+        this.usersList.push(new User(name, pass));
+        localStorage.setItem("usersList", JSON.stringify(this.usersList));
+      } 
+    };
+  }
